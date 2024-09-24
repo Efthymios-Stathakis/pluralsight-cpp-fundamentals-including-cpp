dfd2e3dcbae020a8360be8e34551ee41fd8b87f3
Efthymios-Stathakis <efstatha@hotmail.com>
2024-09-24 16:23:54 +0200

    m10

---
+++ m10/Person.cpp
@@ -0,0 +1,64 @@
+#include "Person.h"
+#include <iostream>
+
+using std::string;
+using std::cout;
+using std::endl;
+
+Person::Person(): firstname(), lastname()  
+{
+    cout << "Constructing " << firstname << " " << lastname << endl;
+};
+
+Person::Person(string first, string last): firstname(first), lastname(last) 
+{
+    cout << "Constructing " << firstname << " " << lastname << endl;
+};
+
+Person::Person(string first, string last, int arb): firstname(first), lastname(last), rnd_num(arb)
+{
+    cout << "Constructing " << firstname << " " << lastname << endl;
+};
+
+Person::~Person()
+{
+    cout << "Destructing " << firstname << " " << lastname << endl;
+};
+
+
+std::string Person::getName() const
+{
+    string fullname = firstname + " " + lastname;
+    return fullname;
+}
+
+bool Person::operator<(Person const& p) const
+{
+    return rnd_num < p.rnd_num;
+}
+
+bool Person::operator<(int i) const
+{
+    return rnd_num < i;
+}
+
+//  Important note: GetNumber needs to be marked as const, otherwise we cannot call 
+//  a non-const function on a const p instance that we pass to the function
+// So the definition needs to be "int GetNumber() const"
+bool operator<(int i, Person const& p)
+{
+    return i < p.GetNumber();
+}
+
+// bool operator<(Person const& p1, Person const& p2)
+// {
+//     return p1.GetNumber() < p2.GetNumber();
+// }
+
+// To make this work we need to use the public function. If not 
+// possible then we can use the keyword "friend" as follows
+// friend bool operator<(int i, Person const& p);
+// bool operator<(int i, Person const& p)
+// {
+//     return i < p.rnd_num;
+// }
